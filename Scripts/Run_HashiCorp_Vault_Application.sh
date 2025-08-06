bdb8239b09b20a55949e5a28de303335b9203b96
Zachariah-Mayfield <zachariah-mayfield@outlook.com>
2025-08-05 23:41:43 -0500

    I have updated the documentation and wrote a simple Run_HashiCorp_Vault_Application.sh shell script to run and setup everything.

---
+++ Scripts/Run_HashiCorp_Vault_Application.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# ~/GitHub/Main/HashiCorp_Vault/Scripts/Run_HashiCorp_Vault_Application.sh
+
+if [[ -n "$VIRTUAL_ENV" ]]; then
+  echo "You are in a Python virtual environment: $VIRTUAL_ENV"
+else
+  echo "You are NOT in a Python virtual environment."
+  echo "Please activate your Python virtual environment before running this script."
+  echo "Run the following to activate your Python Virtual Environment: source ~/GitHub/Main/HashiCorp_Vault/venv/bin/activate"
+  exit 1
+fi
+
+FILE=~/"GitHub/Main/HashiCorp_Vault/.env"
+INIT_FILE=~/"GitHub/Main/HashiCorp_Vault/vault/data/vault-init.json"
+
+# Check if the .env file exists, if not create it
+if [ ! -f "$FILE" ]; then
+  #touch "$FILE"
+  echo "The .env file '$FILE' created."
+else
+  echo "The .env file '$FILE' already exists."
+fi
+
+# Start the HashiCorp Vault application using Docker Compose
+docker-compose down
+
+# This tells Docker Compose to run the containers in the background (“detached mode”).
+docker-compose up -d --build
+
+# Wait for the containers to start
+echo "Waiting for containers to start..."
+wait_time=30 # seconds
+
+# Get the logs from the Vault container
+docker-compose logs -f vault_server
+
+# Extract the Vault root token from the initialization file.
+ROOT_TOKEN=$(jq -r '.root_token' "$INIT_FILE")
+# Login with root token
+# VAULT_TOKEN="$ROOT_TOKEN" vault login "$ROOT_TOKEN" > /dev/null
+
+echo "Check ~/GitHub/Main/HashiCorp_Vault/vault/data/vault-init.json for the root token."
+echo "The token is: ${ROOT_TOKEN}"
+echo "You can use this token to access the Vault UI at http://localhost:8200."
